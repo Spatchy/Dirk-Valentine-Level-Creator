07a550d3274d5e262dd731afd3ac38dab69e0da2
Spatchy <spatchy@spatchy.net>
2021-10-11 23:03:33 +0100

    added new handlers

--- src/ipcHandler.js
+++ src/ipcHandler.js
@@ -1,9 +1,21 @@
-import { shell, ipcMain } from "electron"
+import { shell, ipcMain, dialog, mainWindow } from "electron"
+import {updateSetting} from "./settingsManager";
+import Path from "path";
 
 export default {
 
   openExternal: ipcMain.on("OPEN_EXTERNAL", (event, payload) => {
     shell.openExternal(payload);
+  }),
+
+  openFileDialog: ipcMain.on("OPEN_FILE_DIALOG", async (event, payload) => {
+    const result = await dialog.showOpenDialog(mainWindow, payload);
+    event.reply("OPEN_FILE_DIALOG", result);
+  }),
+
+  updateflashpointDir: ipcMain.on("UPDATE_FLASHPOINT_DIR", (event, payload) => {
+    console.log(Path.dirname(payload[1].path))
+    updateSetting(payload[0], Path.dirname(payload[1].path));
   })
 
 }
