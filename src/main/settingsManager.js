652daea10d223aaf6d751fea137152b79727f823
Spatchy <spatchy@spatchy.net>
2021-10-16 18:06:10 +0100

    added swftoolsPath setting and various corrections

--- src/main/settingsManager.js
+++ src/main/settingsManager.js
@@ -6,6 +6,7 @@ const settingsFile = appdata + "/settings.json"
 
 const defaults = {
   "flashpointPath":"",
+  "swftoolsPath":""
 }
 
 let settingsCache = {};
@@ -17,11 +18,11 @@ export default {
   // returns settings from the file or the defaults if file doesn't exist
   getSettings() {
     let settings;
-    if (settingsCache != {}) {
+    if (Object.keys(settingsCache).length > 0) {
       return settingsCache
     }
     else if (fs.existsSync(settingsFile)) {
-      settings = fs.readFileSync(settingsFile);
+      settings = JSON.parse(fs.readFileSync(settingsFile));
     }
     else {
       settings = defaults;
@@ -30,14 +31,16 @@ export default {
   },
   
   updateSetting(setting, option) {
-    let activeSettings = getSettings();
+    let activeSettings = this.getSettings();
     activeSettings[setting] = option;
     settingsCache = activeSettings;
     fs.writeFileSync(settingsFile, JSON.stringify(activeSettings));
   },
 
   getSetting(settingToGet) {
-    return settingsCache[settingToGet];
+    const settings = this.getSettings();
+    console.log(settings);
+    return settings[settingToGet];
   }
   
 }
