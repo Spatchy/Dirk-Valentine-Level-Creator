514fa67b13caa226736a155fcce7b0df0fce7807
Spatchy <spatchy@spatchy.net>
2021-10-19 19:00:31 +0100

    added swftools download link & complete setup btn

--- src/renderer/views/init.vue
+++ src/renderer/views/init.vue
@@ -3,9 +3,11 @@
     To start, please select your Flashpoint Core folder. You can download Flashpoint Core from <a href="_blank" @click="openInBrowser(`https://bluemaxima.org/flashpoint/downloads/`)"> the official site </a>
     <button @click="getDirectoryPath('flashpointPath')">Select Flashpoint Folder</button>
     <div v-if="flashpointPath">{{flashpointPath}}</div>
+    Please select your SWFtools folder. You can download the latest version from <a href="_blank" @click="openInBrowser(`http://www.swftools.org/swftools-0.9.0.exe`)"></a>
     <button @click="getDirectoryPath('swftoolsPath')">Select SWFtools Folder</button>
     <div v-if="swftoolsPath">{{swftoolsPath}}</div>
-    <button @click="testCommand">Test command</button>
+    To complete setup, the SWF will be downloaded and extracted
+    <button @click="getSwf()">Complete Setup</button>
   </div>
 </template>
 
@@ -38,8 +40,8 @@ export default {
       window.ipc.send("OPEN_FILE_DIALOG", {properties: ['openDirectory']});
     },
 
-    testCommand() {
-      window.ipc.send("TEST", "test");
+    getSwf() {
+      window.ipc.send("DOWNLOAD_EXTRACT_SWF", "");
     }
   }
 }
