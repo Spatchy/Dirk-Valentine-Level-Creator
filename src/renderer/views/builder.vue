78aa74b5b7c355fcb5f709dc8b789ecd79dda018
Spatchy <spatchy@spatchy.net>
2021-11-03 01:46:12 +0000

    implemented tile layer merging on frontend

--- src/renderer/views/builder.vue
+++ src/renderer/views/builder.vue
@@ -2,6 +2,7 @@
   <div>
     <button @click="test()">trigger test</button>
     <div>{{tileset.length}}</div>
+    <img :src="mergeTest" alt="">
   </div>
 </template>
 
@@ -10,11 +11,19 @@ export default {
   data() {
     return {
       tileset: [],
+      mergeTest: null,
     }
   },
   mounted() {
     window.ipc.on("GET_TILES", response => {
       this.tileset.push(response);
+      if(this.tileset.length == 52){
+        console.log(this.tileset);
+        window.mergeImages.merge(["data:image/png;base64," + this.tileset[19].tileImage, "data:image/png;base64," + this.tileset[40].tileImage], (b64) => {
+          this.mergeTest = b64;
+        });
+        console.log(this.mergeTest);
+      }
     })
   },
   methods: {
