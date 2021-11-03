30dde13292141915c95c10e8a7aaca62f407ede7
Spatchy <spatchy@spatchy.net>
2021-11-03 18:50:36 +0000

    created tileDisplay component to hold tile data

---
+++ src/renderer/components/tileDisplay.vue
@@ -0,0 +1,43 @@
+<template>
+  <img :src="tile.tileImage" :title="tile.name" :style="cssProps" :class="{ selected: isSelected }" @click="select()">
+</template>
+
+<script>
+export default {
+  name: "tileDisplay",
+  props: {
+    tile: Object,
+    selectedTile: Number
+  },
+  emits: [
+    "selected"
+  ],
+  computed: {
+    isSelected() {
+      return this.selectedTile === this.tile.number
+    },
+    cssProps() {
+      return {
+        "--tile-width": `${64*this.tile.width}px`,
+        "--tile-height": `${64*this.tile.height}px`,
+      }
+    }
+  },
+  methods: {
+    select() {
+      this.$emit('selected', this.tile.number)
+    }
+  }
+}
+</script>
+
+<style scoped>
+  img {
+    width: var(--tile-width);
+    height: var(--tile-height);
+    padding: 2px;
+  }
+  .selected {
+    background-color: blue;
+  }
+</style>
