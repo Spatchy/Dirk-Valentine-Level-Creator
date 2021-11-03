ca2290e7ba1146d57738e436d7db1931d989c867
Spatchy <spatchy@spatchy.net>
2021-11-03 18:48:04 +0000

    created tile menu component

---
+++ src/renderer/components/tileMenu.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="tile-menu">
+    <tile-display v-for="tile in tileSet" :key="tile.number" :tile="tile" :selectedTile="selectedTile" @selected="selectTile($event)"></tile-display>
+  </div>
+</template>
+
+<script>
+import tileDisplay from './tileDisplay.vue'
+export default {
+  name: "tileMenu",
+  props: {
+    tileSet: Array,
+    selectedTile: Number
+  },
+  components: {
+    "tile-display": tileDisplay
+  },
+  methods: {
+    selectTile(tileId) {
+      this.$emit("selectTile", tileId)
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .tile-menu {
+    width: 33%;
+  }
+</style>
