a8dcee8db53f4f6da9d1f46e0d842ae4be6a05d0
Spatchy <spatchy@spatchy.net>
2021-11-04 15:27:31 +0000

    created level canvas component

---
+++ src/renderer/components/levelCanvas.vue
@@ -0,0 +1,21 @@
+<template>
+  <canvas ref="levelCanvas" :height="canvasHeight" :width="canvasWidth"></canvas>
+</template>
+
+<script>
+export default {
+  name: "levelCanvas",
+  props: {
+    tilesWidth: Number,
+    tilesHeight: Number
+  },
+  computed: {
+    canvasWidth() {
+      return this.tilesWidth * 64
+    },
+    canvasHeight() {
+      return this.tilesHeight * 64
+    }
+  }
+}
+</script>
