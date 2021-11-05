ea4225708c32cb50d8415c34832edba37df45827
Spatchy <spatchy@spatchy.net>
2021-11-05 19:02:04 +0000

    added project menu to show on app startup

---
+++ src/renderer/views/projectMenu.vue
@@ -0,0 +1,26 @@
+<template>
+  <input type="text" v-model="newLevelWidth" placeholder="width">
+  <input type="text" v-model="newLevelHeight" placeholder="height">
+  <button @click="newProject()">Create Project</button>
+</template>
+
+<script>
+export default {
+  name: "projectMenu",
+  data() {
+    return {
+      newLevelWidth: null,
+      newLevelHeight: null,
+    }
+  },
+  emits: [
+    "createNew",
+  ],
+  methods: {
+    newProject() {
+      window.ipc.send("CREATE_LEVEL", [this.newLevelWidth, this.newLevelHeight])
+      this.$emit("createNew", [this.newLevelWidth, this.newLevelHeight])
+    }
+  }
+}
+</script>
