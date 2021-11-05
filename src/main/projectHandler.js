885502e76f2e838d94b2c644654e5060787cbe0f
Spatchy <spatchy@spatchy.net>
2021-11-05 17:29:58 +0000

    Implemented tile insertion and data retrieval

--- src/main/projectHandler.js
+++ src/main/projectHandler.js
@@ -1,22 +1,36 @@
-import settingsManager from "./settingsManager";
-
 class levelData {
   constructor(width, height) {
     this.levelArray = this.make2dArray(width, height)
   }
   make2dArray(width, height) {
     const arr = []
-    for(i = 0; i < height; i++) {
+    for(let i = 0; i < height; i++) {
       const row = []
-      for(i = 0; i < width; i++) {
+      for(let i = 0; i < width; i++) {
         row.push(0)
       }
       arr.push(row)
     }
     return arr
   }
+  insertTile(tileId, x, y) {
+    console.log("id: " + tileId + " x: " + x + " y: " + y)
+    this.levelArray[y][x] = tileId
+  }
+  getLevelArray() {
+    return this.levelArray
+  }
 }
 
-export default {
+let activeLevel = null;
 
+export default {
+  getActiveLevel() {
+    return activeLevel
+  },
+
+  createLevel(width, height) {
+    console.log("CREATING LEVEL")
+    activeLevel = new levelData(width, height)
+  }
 }
