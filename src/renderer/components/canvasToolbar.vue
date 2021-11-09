6507e1494f4ae87e7aa74cd71372aa931e95b350
Spatchy <spatchy@spatchy.net>
2021-11-09 22:20:31 +0000

    created canvas toolbar with background selection

---
+++ src/renderer/components/canvasToolbar.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <label for="backgroundSelect">Background Style: </label>
+    <select :value="initialisedIsOutside" @change="changeBackground($event)" id="backgroundSelect">
+      <option v-for="n in backgroundOptions" :key="n.value" :value="n.value">{{n.name}}</option>
+    </select>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    initialisedIsOutside: Boolean,
+  },
+  emits: [
+    "changeBackground",
+  ],
+  data() {
+    return {
+      backgroundOptions: [
+        {value: true, name: "Outside"},
+        {value: false, name: "Inside"},
+      ],
+    }
+  },
+  methods: {
+    changeBackground(event) {
+      this.$emit("changeBackground", event.target.value)
+    }
+  }
+}
+</script>
