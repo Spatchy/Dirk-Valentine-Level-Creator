ec11a1b6fa94d807ed40c8dc012c24224f082f96
Spatchy <spatchy@spatchy.net>
2021-11-10 00:49:12 +0000

    implemented painting canvas background

--- src/renderer/components/levelCanvas.vue
+++ src/renderer/components/levelCanvas.vue
@@ -1,10 +1,10 @@
 <template>
   <canvas-toolbar
-    :initialisedIsOutside="true"
+    :initialisedIsOutside="initialisedIsOutside"
     @changeBackground="changeBackground($event)"
   />
   <div ref="canvasStack" id="canvasStack">
-    <canvas ref="backGroundCanvas" :height="canvasHeight" :width="canvasWidth" style="z-index: -1"></canvas>
+    <canvas ref="backgroundCanvas" :height="canvasHeight" :width="canvasWidth" style="z-index: -1"></canvas>
     <canvas v-for="(n, i) in numberOfLayers" :key="i" :ref="`canvasLayer${i}`" :height="canvasHeight" :width="canvasWidth" :style="`z-index: ${i}`"></canvas>
     <canvas ref="selectionCanvas" :height="canvasHeight" :width="canvasWidth" @click="canvasAction($event)" style="z-index: 99999"></canvas>
   </div>
@@ -20,6 +20,8 @@ export default {
   data() {
     return {
       numberOfLayers: 3,
+      initialisedIsOutside: false,
+      backgroundImageData: null,
     }
   },
   props: {
@@ -36,6 +38,14 @@ export default {
       return this.tilesHeight * 64
     }
   },
+  mounted() {
+    window.ipc.send("GET_BACKGROUND_IMAGE_DATA", "")
+
+    window.ipc.on("GET_BACKGROUND_IMAGE_DATA", response => {
+      this.backgroundImageData = response
+      this.paintBackgroundCanvas(this.initialisedIsOutside)
+    })
+  },
   methods: {
     canvasAction(event) {
       const xy = this.getCursorPosition(event)
@@ -47,7 +57,7 @@ export default {
     },
 
     getCursorPosition(event) {
-      const canvas = this.$refs.levelCanvas
+      const canvas = this.$refs.selectionCanvas
       const rect = canvas.getBoundingClientRect()
       const x = event.clientX - rect.left
       const y = event.clientY - rect.top
@@ -58,19 +68,47 @@ export default {
       return Math.floor(pos/64)*64
     },
 
-    placeTile(tile, x, y) {
+    placeTile(tile, x, y) { // change this to reflect layering system
       const ctx = this.$refs.levelCanvas.getContext("2d")
       const imageElem = new Image()
       imageElem.src = tile.tileImage
       ctx.drawImage(imageElem, x, y)
       window.ipc.send("INSERT_TILE", [tile.number, x/64, y/64])
     },
+
     addLayer() {
       this.numberOfLayers++
     },
+
     changeBackground(isOutside) {
       console.log(isOutside)
-    }
+      this.paintBackgroundCanvas(isOutside)
+    },
+
+    paintBackgroundCanvas(isOutside) {
+      const arrToUse = [this.backgroundImageData[0]]
+      if(isOutside) {
+        arrToUse.push(this.backgroundImageData[1])
+      } else {
+        arrToUse.push(this.backgroundImageData[2])
+      }
+      const bgCanvas = this.$refs.backgroundCanvas
+      const ctx = bgCanvas.getContext("2d")
+      arrToUse.forEach(image => {
+        const imageElem = new Image()
+        imageElem.src = image
+        imageElem.addEventListener("load", () => {
+          console.log("load successful")
+          const pattern = ctx.createPattern(imageElem, "repeat")
+          ctx.fillstyle = pattern
+          ctx.fillRect(0, 0, bgCanvas.width, bgCanvas.height)
+        })
+        imageElem.addEventListener("error", (e) => {
+          console.error(e)
+        })
+      });
+      
+    },
   }
 }
 </script>
