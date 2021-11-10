e7d3410098ea3e30dba05e46921ebb6d6aa6809b
Spatchy <spatchy@spatchy.net>
2021-11-10 15:45:53 +0000

    implemented layer selection

--- src/renderer/components/canvasToolbar.vue
+++ src/renderer/components/canvasToolbar.vue
@@ -4,6 +4,10 @@
     <select :value="initialisedIsOutside" @change="changeBackground($event)" id="backgroundSelect">
       <option v-for="n in backgroundOptions" :key="n.value" :value="n.value">{{n.name}}</option>
     </select>
+    <label for="layerSelect">Select Layer</label>
+    <select :value="0" id="LayerSelect" @change="changeLayerSelection($event)">
+      <option v-for="(n, i) in numberOfLayers" :key="i" :value="i">{{`Layer ${n}`}}</option>
+    </select>
   </div>
 </template>
 
@@ -11,9 +15,11 @@
 export default {
   props: {
     initialisedIsOutside: Boolean,
+    numberOfLayers: Number,
   },
   emits: [
     "changeBackground",
+    "changeLayerSelection"
   ],
   data() {
     return {
@@ -26,6 +32,10 @@ export default {
   methods: {
     changeBackground(event) {
       this.$emit("changeBackground", event.target.value)
+    },
+    
+    changeLayerSelection(event) {
+      this.$emit("changeLayerSelection", event.target.value)
     }
   }
 }
