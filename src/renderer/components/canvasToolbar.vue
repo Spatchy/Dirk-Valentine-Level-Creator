ead787e063d079b959063182e63472315a8a07f2
Spatchy <spatchy@spatchy.net>
2021-11-10 18:58:46 +0000

    added save and new layer functions to toolbar

--- src/renderer/components/canvasToolbar.vue
+++ src/renderer/components/canvasToolbar.vue
@@ -8,6 +8,8 @@
     <select :value="0" id="LayerSelect" @change="changeLayerSelection($event)">
       <option v-for="(n, i) in numberOfLayers" :key="i" :value="i">{{`Layer ${n}`}}</option>
     </select>
+    <button @click="addLayer()">+ New Layer</button>
+    <button @click="saveLevel()">save level</button>
   </div>
 </template>
 
@@ -15,11 +17,12 @@
 export default {
   props: {
     initialisedIsOutside: Boolean,
-    numberOfLayers: Number,
+    initialNumberOfLayers: Number,
   },
   emits: [
     "changeBackground",
-    "changeLayerSelection"
+    "changeLayerSelection",
+    "addLayer",
   ],
   data() {
     return {
@@ -27,15 +30,26 @@ export default {
         {value: true, name: "Outside"},
         {value: false, name: "Inside"},
       ],
+      numberOfLayers: null
     }
+  },
+  mounted() {
+    this.numberOfLayers = this.initialNumberOfLayers
   },
   methods: {
     changeBackground(event) {
       this.$emit("changeBackground", event.target.value)
     },
-    
+
     changeLayerSelection(event) {
       this.$emit("changeLayerSelection", event.target.value)
+    },
+    addLayer() {
+      this.$emit("addLayer")
+      this.numberOfLayers++
+    },
+    saveLevel() {
+      window.ipc.send("SAVE_LEVEL", "")
     }
   }
 }
