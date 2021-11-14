650415d5ba3a399ab9d5e0d3b817e50adae849a2
Spatchy <spatchy@spatchy.net>
2021-11-14 23:00:18 +0000

    formatted or something idk

--- src/renderer/views/builder.vue
+++ src/renderer/views/builder.vue
@@ -1,25 +1,35 @@
 <template>
   <div>
     <div class="box">
-      <div>{{tileset.length}}</div>
-      <img :src="mergeTest" alt="">
-      <h1>{{newProjectName}}</h1>
-      <h6>level {{newLevelNum}}</h6>
+      <div>{{ tileset.length }}</div>
+      <img :src="mergeTest" alt="" />
+      <h1>{{ newProjectName }}</h1>
+      <h6>level {{ newLevelNum }}</h6>
     </div>
     <div class="columns">
       <div class="column is-one-third">
-        <tile-menu :tileSet="tileset" :selectedTile="selectedTile" @selectTile="changeSelectedTile($event)"> </tile-menu>
+        <tile-menu
+          :tileSet="tileset"
+          :selectedTile="selectedTile"
+          @selectTile="changeSelectedTile($event)"
+        >
+        </tile-menu>
       </div>
       <div class="column">
-        <level-canvas :tilesWidth="newLevelWidth" :tilesHeight="newLevelHeight" :selectedTile="selectedTile" :tileSet="tileset"></level-canvas>
+        <level-canvas
+          :tilesWidth="newLevelWidth"
+          :tilesHeight="newLevelHeight"
+          :selectedTile="selectedTile"
+          :tileSet="tileset"
+        ></level-canvas>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import tileMenu from "../components/tileMenu.vue"
-import levelCanvas from "../components/levelCanvas.vue"
+import tileMenu from "../components/tileMenu.vue";
+import levelCanvas from "../components/levelCanvas.vue";
 
 export default {
   name: "builder",
@@ -29,7 +39,7 @@ export default {
       mergeTest: null,
       selectedTile: 0,
       selectedLayer: 0,
-    }
+    };
   },
   props: {
     newLevelWidth: Number,
@@ -43,24 +53,27 @@ export default {
     levelCanvas,
   },
   mounted() {
-    window.ipc.send("GET_TILES", "")
-    console.log(this.newLevelWidth)
-    console.log(this.newLevelHeight)
+    window.ipc.send("GET_TILES", "");
+    console.log(this.newLevelWidth);
+    console.log(this.newLevelHeight);
 
-    window.ipc.on("GET_TILES", response => {
+    window.ipc.on("GET_TILES", (response) => {
       this.tileset.push(response);
-      if(this.tileset.length === 353){
-        window.mergeImages.merge([this.tileset[19].tileImage, this.tileset[40].tileImage], (b64) => {
-          this.mergeTest = b64;
-        });
-        window.ipc.send("OPEN_LEVEL_DATA", this.newDir)
+      if (this.tileset.length === 353) {
+        window.mergeImages.merge(
+          [this.tileset[19].tileImage, this.tileset[40].tileImage],
+          (b64) => {
+            this.mergeTest = b64;
+          }
+        );
+        window.ipc.send("OPEN_LEVEL_DATA", this.newDir);
       }
-    })
+    });
   },
   methods: {
     changeSelectedTile(tileId) {
-      this.selectedTile = tileId
+      this.selectedTile = tileId;
     },
-  }
-}
+  },
+};
 </script>
