6eb3110ff96c703c2941f6fcf4ec625a476ca73e
Spatchy <spatchy@spatchy.net>
2021-11-14 21:43:00 +0000

    added experimental styling

--- src/renderer/views/projectMenu.vue
+++ src/renderer/views/projectMenu.vue
@@ -1,15 +1,21 @@
 <template>
-  <div v-for="n in projectsList" :key="n[0]" :id="n[0]" :ref="`projectItem${n[0]}`" @click="toggleExpandProject($event, n[0])">
-    {{n[0]}}
+  <div class="columns is-centered">
+    <div class="column is-half full-height">
+      <div class="column-scrollable">
+        <div v-for="n in projectsList" :key="n[0]" :id="n[0]" :ref="`projectItem${n[0]}`" @click="toggleExpandProject($event, n[0])" class="notification">
+          <h4 class="is-size-4">{{n[0]}}</h4>
 
-    <div v-if="expand == n[0]">
-      <p v-for="i in n[1]" :key="`${n[0]}:${i}`" @click="openLevel(n[0], i)">{{`Level ${i}`}}</p>
+          <div v-if="expand == n[0]">
+            <p v-for="i in n[1]" :key="`${n[0]}:${i}`" @click="openLevel(n[0], i)">{{`Level ${i}`}}</p>
+            <button @click="newLevel(n[0])" class="button is-small">+ Add level</button>
+          </div>
+
+        </div>
+      </div>
+      <input type="text" v-model="newLevelWidth" placeholder="width">
+      <input type="text" v-model="newLevelHeight" placeholder="height">
     </div>
-
-    <button @click="newLevel(n[0])">+ Add level</button>
   </div>
-  <input type="text" v-model="newLevelWidth" placeholder="width">
-  <input type="text" v-model="newLevelHeight" placeholder="height">
 </template>
 
 <script>
@@ -49,9 +55,20 @@ export default {
       this.$emit("openExisting", [project, levelNum])
     },
 
-    toggleExpandProject(event, project){
-      this.expand = project
+    toggleExpandProject(event, project) {
+      if(this.expand === project) {
+        this.expand = null
+      } else {
+        this.expand = project
+      }
     }
   }
 }
 </script>
+
+<style lang="scss" scoped>
+  .column-scollable {
+    height: 400px;
+    overflow: auto;
+  }
+</style>
